0ba16e6c9dbe06c2ae16a5bbafe882137a58bc77
Myungsu Woo <master.sy90@gmail.com>
2017-09-25 16:16:39 +0900

    add doPost() method

--- web-study-09/src/com/saeyan/controller/LoginServlet.java
+++ web-study-09/src/com/saeyan/controller/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.saeyan.dao.MemberDAO;
+import com.saeyan.dto.MemberVO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -40,6 +44,25 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String url = "member/login.jsp";
+		String userid = request.getParameter("userid");
+		String pwd = request.getParameter("pwd");
+		MemberDAO mDao = MemberDAO.getInstance();
+		int result = mDao.userCheck(userid, pwd);
+		
+		if (result == 1) {
+			MemberVO mVo = mDao.getMember(userid);
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mVo);
+			request.setAttribute("message", "로그인에 성공했습니다.");
+			url = "main.jsp";
+		} else if (result == 0) {
+			request.setAttribute("message", "비밀번호가 맞지 않습니다.");
+		} else if (result == -1) {
+			request.setAttribute("message", "존재하지 않는 회원입니다.");
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
-
 }
