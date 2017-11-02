104871a2e5970cbfd17b3cf75d7ff11551ae6f0a
Myungsu Woo <master.sy90@gmail.com>
2017-11-02 17:25:03 +0900

    login status

--- web-study-09/src/com/saeyan/controller/LoginServlet.java
+++ web-study-09/src/com/saeyan/controller/LoginServlet.java
@@ -34,7 +34,14 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("member/login.jsp");
+		String url = "member/login.jsp";
+		HttpSession session = request.getSession();
+		
+		if (session.getAttribute("loginUser") != null) {	// 이미 로그인 된 사용자이면
+			url = "main.jsp";	// 메인 페이지로 이동한다.
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		
 		dispatcher.forward(request, response);
 	}
 
