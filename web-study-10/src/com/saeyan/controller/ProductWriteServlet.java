ccd057e04724ae1d443a56645d2176c4cff610b7
Myungsu Woo <master.sy90@gmail.com>
2017-11-24 02:14:01 +0900

    define doPost() method for product write

--- web-study-10/src/com/saeyan/controller/ProductWriteServlet.java
+++ web-study-10/src/com/saeyan/controller/ProductWriteServlet.java
@@ -3,11 +3,17 @@ package com.saeyan.controller;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+import com.saeyan.dao.ProductDAO;
+import com.saeyan.dto.ProductVO;
 
 /**
  * Servlet implementation class ProductWriteServlet
@@ -36,8 +42,31 @@ public class ProductWriteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		ServletContext context = getServletContext();
+		String path = context.getRealPath("upload");
+		String encType="UTF-8";
+		int sizeLimit = 20 * 1024 * 1024;
+		
+		MultipartRequest multi = new MultipartRequest(request, path, sizeLimit, 
+				encType, new DefaultFileRenamePolicy());
+		
+		String name = multi.getParameter("name");
+		int price = Integer.parseInt(multi.getParameter("price"));
+		String description = multi.getParameter("description");
+		String pictureUrl = multi.getFilesystemName("pictureUrl");
+		
+		ProductVO pVo = new ProductVO();
+		pVo.setName(name);
+		pVo.setPrice(price);
+		pVo.setDescription(description);
+		pVo.setPictureUrl(pictureUrl);
+		
+		ProductDAO pDao = ProductDAO.getInstance();
+		pDao.insertProduct(pVo);
+		
+		response.sendRedirect("productList.do");
 	}
 
 }
